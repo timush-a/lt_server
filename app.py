78316b6bcd1113d90048a5837cf574526416eae7
timush-a <smtimush@yandex.ru>
2023-10-08 02:36:41 +0300

    added set up method

--- app.py
+++ app.py
@@ -1,26 +1,35 @@
+import asyncio
+import logging
+
 from aiohttp import web
 from argparse import ArgumentParser
 from handlers import LTHandlers
-from common.utils import get_logger, load_config
+from common.utils import get_logger, load_json, set_up
 
 
-def get_cmd_args():
+def get_cmd_args() -> str:
     parser = ArgumentParser()
     parser.add_argument('-c', '--config', type=str, help='Config path')
     args = parser.parse_args()
     return args.config
 
 
-def create_app(logger) -> web.Application:
-    lt_handdlers = LTHandlers(logger)
-    app = web.Application()
-    app.add_routes(lt_handdlers.routes)
-    return app
+def create_web_app(logger: logging.Logger, app_config: dict) -> web.Application:
+    lt_handdlers = LTHandlers(logger, app_config)
+    for route in lt_handdlers.routes:
+        logger.info(f"App route {route.path}")
+    web_app = web.Application()
+    web_app.add_routes(lt_handdlers.routes)
+    return web_app
 
 
 if __name__ == "__main__":
     config_path = get_cmd_args()
-    config = load_config(config_path)
+    config = load_json(config_path)
+
+    set_up(config)
+
     _logger = get_logger(filename=config['logs_path'])
-    app = create_app(_logger)
+    app = create_web_app(_logger, config)
+
     web.run_app(app, host=config['host'], port=config['port'], access_log=None)
